a36aa50657e2bcb7600c499612a4b8cf711a39a6
c.hülsmann <115979110+Chippzzii@users.noreply.github.com>
2023-03-07 08:46:29 +0100

    update

--- main.py
+++ main.py
@@ -1,29 +1,73 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("MitarbeiterDatei")
-window.minsize(width=800, height=600)
-window.resizable(width=False, height=False)
-hauptframe = tk.Frame(width=300, height=300, bg="red")
-zweitesFrame = tk.Frame(width=300, height=300)
-vorname = tk.Label(master=hauptframe, text="Vorname")
-vorname.pack()
-eingabe = tk.Entry(master=hauptframe)
-eingabe.pack()
-nachname = tk.Label(master=hauptframe, text="Nachname")
-nachname.pack()
-nameEingabe = tk.Entry(master=hauptframe)
-nameEingabe.pack()
-button_a = tk.Button(master=hauptframe, text="Hinzufügen")
-button_a.pack()
+mitarbeiter = tk.Tk()
+mitarbeiter.title("MitarbeiterDatei")
+mitarbeiter.minsize(width=600, height=300)
+mitarbeiter.resizable(width=False, height=False)
+#---------------------------------------------------------
+#Frame und Widgets für die Eingabe
+eingabeFrame = tk.Frame(width=200, height=298, bg="red")
+nummerEntry = tk.Entry(master=eingabeFrame, width=20, font="ComicSans")
+vornameEntry = tk.Entry(master=eingabeFrame, width=20, font="ComicSans")
+nachnameEntry = tk.Entry(master=eingabeFrame, width=20, font="ComicSans")
+gebDatumEntry = tk.Entry(master=eingabeFrame, width=20, font="ComicSans")
+vornameLabel = tk.Label(master=eingabeFrame, text="Vorname")
+nachnameLabel = tk.Label(master=eingabeFrame, text="Nachname")
+nummerLabel = tk.Label(master=eingabeFrame, text="Laufende Nummer")
+gebDatumLabel = tk.Label(master=eingabeFrame, text="Geburtsdatum")
 
-label_a = tk.Label(master=hauptframe, text="frame A")
-label_a.pack()
-
-label_b = tk.Label(master=zweitesFrame, text="frame B")
-label_b.pack()
-hauptframe.place(x=50, y=75)
-zweitesFrame.pack()
+#---------------------------------------------------------
+#Frame für Buttons und AnzigeListe der Personen
+anzeigeFrame = tk.Frame(width=397, height=298, bg="red")
+anzeigeBox = tk.Listbox(master=anzeigeFrame)
+anzeigeBox.place(x=5, y=5)
 
 
-window.mainloop()
+#---------------------------------------------------------
+#PlaceBereich
+nummerLabel.place(x=10, y=30)
+nummerEntry.place(x=10, y=55)
+vornameLabel.place(x=10, y=80)
+vornameEntry.place(x=10, y=105)
+nachnameLabel.place(x=10, y=130)
+nachnameEntry.place(x=10, y=155)
+gebDatumLabel.place(x=10, y=180)
+gebDatumEntry.place(x=10, y=205)
+
+
+eingabeFrame.place(x=1, y=1)
+anzeigeFrame.place(x=202, y=1)
+mitarbeiter.mainloop()
+
+# def add():
+
+
+
+#
+# window = tk.Tk()
+# window.title("MitarbeiterDatei")
+# window.minsize(width=800, height=600)
+# window.resizable(width=False, height=False)
+# hauptframe = tk.Frame(width=300, height=300, bg="red")
+# zweitesFrame = tk.Frame(width=300, height=300)
+# vorname = tk.Label(master=hauptframe, text="Vorname")
+# vorname.pack()
+# eingabe = tk.Entry(master=hauptframe)
+# eingabe.pack()
+# nachname = tk.Label(master=hauptframe, text="Nachname")
+# nachname.pack()
+# nameEingabe = tk.Entry(master=hauptframe)
+# nameEingabe.pack()
+# button_a = tk.Button(master=hauptframe, text="Hinzufügen")
+# button_a.pack()
+#
+# label_a = tk.Label(master=hauptframe, text="frame A")
+# label_a.pack()
+#
+# label_b = tk.Label(master=zweitesFrame, text="frame B")
+# label_b.pack()
+# hauptframe.place(x=50, y=75)
+# zweitesFrame.pack()
+#
+#
+# window.mainloop()
